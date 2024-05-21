b676dd3d05fa61face773ac902c84cc4a4ac19d4
MRaihanZ <mraihanzhafran.14@gmail.com>
2024-05-22 05:44:08 +0700

    feat: adding dashboard pages (UI not being develope yet)

---
+++ src/pages/dashboard.jsx
@@ -0,0 +1,14 @@
+import { A } from "@solidjs/router";
+import pertaminaLogo from "/src/assets/img/pertamina_logo.png";
+
+function ForgotPw() {
+	return (
+		<>
+			<div class="container mx-auto p-8 flex items-center h-screen">
+				<div class="mx-auto max-w-md">{/* <!-- ... --> */}</div>
+			</div>
+		</>
+	);
+}
+
+export default ForgotPw;
