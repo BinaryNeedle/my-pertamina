7293ff3a558e49e962e51763cf3087e2410e34f0
MRaihanZ <101298615+MRaihanZ@users.noreply.github.com>
2024-05-23 16:21:41 +0700

    feat: adding qrcode generator in dashboard page

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,14 +1,25 @@
+import { createSignal } from "solid-js";
 import { A } from "@solidjs/router";
 import pertaminaLogo from "/src/assets/img/pertamina_logo.png";
+import { toDataURL } from "qrcode";
 
-function ForgotPw() {
+const [qr, setqr] = createSignal("");
+toDataURL("I am a pony!")
+	.then(setqr)
+	.catch((err) => {
+		console.error(err);
+	});
+
+function Dashboard() {
 	return (
 		<>
 			<div class="container mx-auto px-8 mt-8">
-				<div class="bg-white drop-shadow-lg rounded-lg h-screen"></div>
+				<div class="bg-white drop-shadow-lg rounded-lg h-screen">
+					<img src={qr()}></img>
+				</div>
 			</div>
 		</>
 	);
 }
 
-export default ForgotPw;
+export default Dashboard;
