eb648ae3cc89a40b392886a45b9c26a37d924ca2
Rai <101298615+MRaihanZ@users.noreply.github.com>
2024-05-24 06:03:38 +0700

    feat: adding icon Balance in dashboard (not finish yet)

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -20,8 +20,23 @@ function Dashboard() {
 		<>
 			<MainLayout showNavbar={true} showFooter={true}>
 				<section class="container mx-auto px-8 mt-8">
-					<section className="bg-white drop-shadow-lg rounded-lg flex">
-						Balance
+					<section className="flex justify-center">
+						<span className="bg-white drop-shadow-lg rounded-l-lg ">
+							Balance
+						</span>
+						<span className="bg-white drop-shadow-lg rounded-r-lg ">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="16"
+								height="16"
+								fill="currentColor"
+								class="bi bi-cash-stack"
+								viewBox="0 0 16 16"
+							>
+								<path d="M1 3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1zm7 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4" />
+								<path d="M0 5a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1zm3 0a2 2 0 0 1-2 2v4a2 2 0 0 1 2 2h10a2 2 0 0 1 2-2V7a2 2 0 0 1-2-2z" />
+							</svg>
+						</span>
 					</section>
 					<section class="flex justify-center mt-10">
 						<span className="bg-white drop-shadow-lg rounded-lg p-5">
