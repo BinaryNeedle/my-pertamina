8bdffd95f7c3b1d524d1372c5231fbd8a285b076
MRaihanZ <101298615+MRaihanZ@users.noreply.github.com>
2024-05-23 16:38:43 +0700

    feat: adding example sha256 encryption to qrcode in dashboard page

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -4,7 +4,7 @@ import pertaminaLogo from "/src/assets/img/pertamina_logo.png";
 import { toDataURL } from "qrcode";
 
 const [qr, setqr] = createSignal("");
-toDataURL("I am a pony!")
+toDataURL("2b2ee9d8c90760c616eb9eef56434dd9d323b6ed449204fdc6282b54ebc5d8ff") //example of sha256 encryption(Date+iduser+idpayment+onetimecode)
 	.then(setqr)
 	.catch((err) => {
 		console.error(err);
