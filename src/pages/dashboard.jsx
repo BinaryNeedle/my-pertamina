cb749f654ab47772917d81dcf5e624fd7501bf9d
MRaihanZ <101298615+MRaihanZ@users.noreply.github.com>
2024-05-25 19:40:20 +0700

    feat: change thead bg color from white to gray-50

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -97,7 +97,7 @@ function Dashboard() {
 					<section className={marginFooter()}>
 						<div className="relative overflow-x-auto shadow-md sm:rounded-lg bg-white">
 							<table className="w-full text-sm text-left rtl:text-right text-gray-500">
-								<thead className="text-xs text-gray-700 uppercase">
+								<thead className="text-xs text-gray-700 uppercase bg-gray-50">
 									<tr>
 										<th scope="col" className="px-6 py-3">
 											No
