7c09f4b251be6c615a05f834f6314ba06211f27e
MRaihanZ <101298615+MRaihanZ@users.noreply.github.com>
2024-05-25 19:40:55 +0700

    feat: change color table and data table

--- src/pages/history.jsx
+++ src/pages/history.jsx
@@ -6,15 +6,18 @@ function NotFound() {
 			<MainLayout showNavbar={true} showFooter={true}>
 				<section class="container mx-auto px-8 mt-8">
 					<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-						<table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-							<thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+						<table class="w-full text-sm text-left rtl:text-right text-gray-500">
+							<thead class="text-xs text-gray-700 uppercase bg-gray-50">
 								<tr>
 									<th scope="col" class="px-6 py-3">
-										Product name
+										No
+									</th>
+									<th scope="col" class="px-6 py-3">
+										Id Transaksi
 									</th>
 									<th scope="col" class="px-6 py-3">
 										<div class="flex items-center">
-											Color
+											Nominal
 											<a href="#">
 												<svg
 													class="w-3 h-3 ms-1.5"
@@ -30,7 +33,7 @@ function NotFound() {
 									</th>
 									<th scope="col" class="px-6 py-3">
 										<div class="flex items-center">
-											Category
+											Tanggal Transaksi
 											<a href="#">
 												<svg
 													class="w-3 h-3 ms-1.5"
@@ -43,85 +46,42 @@ function NotFound() {
 												</svg>
 											</a>
 										</div>
-									</th>
-									<th scope="col" class="px-6 py-3">
-										<div class="flex items-center">
-											Price
-											<a href="#">
-												<svg
-													class="w-3 h-3 ms-1.5"
-													aria-hidden="true"
-													xmlns="http://www.w3.org/2000/svg"
-													fill="currentColor"
-													viewBox="0 0 24 24"
-												>
-													<path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z" />
-												</svg>
-											</a>
-										</div>
-									</th>
-									<th scope="col" class="px-6 py-3">
-										<span class="sr-only">Edit</span>
 									</th>
 								</tr>
 							</thead>
 							<tbody>
-								<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+								<tr class="bg-white border-b">
 									<th
 										scope="row"
-										class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+										class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
 									>
-										Apple MacBook Pro 17"
+										1
 									</th>
-									<td class="px-6 py-4">Silver</td>
-									<td class="px-6 py-4">Laptop</td>
+									<td class="px-6 py-4">Apple MacBook Pro 17"</td>
 									<td class="px-6 py-4">$2999</td>
-									<td class="px-6 py-4 text-right">
-										<a
-											href="#"
-											class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-										>
-											Edit
-										</a>
-									</td>
+									<td class="px-6 py-4">Silver</td>
 								</tr>
-								<tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+								<tr class="bg-white border-b">
 									<th
 										scope="row"
-										class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+										class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
 									>
-										Microsoft Surface Pro
+										2
 									</th>
-									<td class="px-6 py-4">White</td>
-									<td class="px-6 py-4">Laptop PC</td>
+									<td class="px-6 py-4">Microsoft Surface Pro</td>
 									<td class="px-6 py-4">$1999</td>
-									<td class="px-6 py-4 text-right">
-										<a
-											href="#"
-											class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-										>
-											Edit
-										</a>
-									</td>
+									<td class="px-6 py-4">White</td>
 								</tr>
-								<tr class="bg-white dark:bg-gray-800">
+								<tr class="bg-white">
 									<th
 										scope="row"
-										class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+										class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
 									>
-										Magic Mouse 2
+										3
 									</th>
-									<td class="px-6 py-4">Black</td>
-									<td class="px-6 py-4">Accessories</td>
+									<td class="px-6 py-4">Magic Mouse 2</td>
 									<td class="px-6 py-4">$99</td>
-									<td class="px-6 py-4 text-right">
-										<a
-											href="#"
-											class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-										>
-											Edit
-										</a>
-									</td>
+									<td class="px-6 py-4">Black</td>
 								</tr>
 							</tbody>
 						</table>
