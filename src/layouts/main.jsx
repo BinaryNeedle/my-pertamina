c7f5c7775f9c546fde7499822cdad226e46b1aa0
MRaihanZ <101298615+MRaihanZ@users.noreply.github.com>
2024-05-27 10:16:55 +0700

    feat: change component navbar and footer name in first character from lower case to upper case

--- src/layouts/main.jsx
+++ src/layouts/main.jsx
@@ -1,19 +1,19 @@
 import { Show } from "solid-js";
 import "/src/assets/css/App.css";
-import Navbar from "../components/navbar";
-import Footer from "../components/footer";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 function MainLayout(props) {
-  const showNavbar = props.showNavbar;
-  const showFooter = props.showFooter;
+	const showNavbar = props.showNavbar;
+	const showFooter = props.showFooter;
 
-  return (
-    <>
-      {showNavbar && <Navbar />}
-      <div className="main-content">{props.children}</div>
-      {showFooter && <Footer />}
-    </>
-  );
+	return (
+		<>
+			{showNavbar && <Navbar />}
+			<div className="main-content">{props.children}</div>
+			{showFooter && <Footer />}
+		</>
+	);
 }
 
 export default MainLayout;
